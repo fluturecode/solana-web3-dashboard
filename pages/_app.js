0b959add79156098e51e7acf6b41f2f4ce7e1d06
fluturecode <fluturecode@gmail.com>
2022-03-09 19:16:04 -0500

    added local next env

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import { MoralisProvider } from 'react-moralis'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <MoralisProvider
+      appId=''
+      serverUrl=''
+    >
+      <Component {...pageProps} />
+    </MoralisProvider>
+  )
 }
 
 export default MyApp
